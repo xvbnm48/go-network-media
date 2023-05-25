7629b7ee44596660e1bdc4eb267f77792a1e7637
fariz.wisnu <fariz.wisnu@bluebirdgroup.com>
2023-05-25 11:29:15 +0700

    formatter for all post

--- post/formatter.go
+++ post/formatter.go
@@ -5,6 +5,7 @@ import "github.com/xvbnm48/go-network-media/model"
 type PostFormatter struct {
 	Title     string `json:"title"`
 	Content   string `json:"content"`
+	Author    string `json:"author"`
 	CreatedAt string `json:"created_at"`
 }
 
@@ -12,7 +13,22 @@ func FormatPost(post model.Post) PostFormatter {
 	postFormat := PostFormatter{}
 	postFormat.Title = post.Title
 	postFormat.Content = post.Content
+	postFormat.Author = post.Author
 	postFormat.CreatedAt = post.CreatedAt.Format("2006-01-02 15:04:05")
 
 	return postFormat
+}
+
+func FormatPosts(posts []model.Post) []PostFormatter {
+	if len(posts) == 0 {
+		return []PostFormatter{}
+	}
+	var postsFormatter []PostFormatter
+
+	for _, post := range posts {
+		postFormatter := FormatPost(post)
+		postsFormatter = append(postsFormatter, postFormatter)
+	}
+
+	return postsFormatter
 }
