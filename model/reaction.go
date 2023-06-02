cc66831deeade540912940f2f5aa3b5ff4c062a7
fariz.wisnu <fariz.wisnu@bluebirdgroup.com>
2023-06-02 17:50:15 +0700

    refactoring from like to vote

--- model/reaction.go
+++ model/reaction.go
@@ -1,9 +1,8 @@
 package model
 
-type Reaction struct {
+type Votes struct {
+	id     int `json:"post_id"`
+	Post   Post
 	UserId int `json:"user_id"`
 	User   User
-	PostId int `json:"post_id"`
-	Post   Post
-	Status bool
 }
